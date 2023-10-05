94c0adafa8f864480f9c322f58498f3957289288
naushinhoque <fnmh@yahoo.com>
2023-10-05 13:34:32 -0500

    code grade push

--- frontend/axios/index.js
+++ frontend/axios/index.js
@@ -1,1 +1,24 @@
 // ✨ implement axiosWithAuth
+import axios from 'axios';
+
+const axiosWithAuth = axios.create({
+    //Set up the base URL for my API
+    baseURL: 'http://localhost:9000/api',
+    //Set headers for every request
+    headers: {
+        'Content-Type': 'application/json'
+    
+    }
+})
+
+axiosWithAuth.interceptors.request.use((config) => {
+    //get token from localSTorage
+    const token = localStorage.getItem('token');
+    //If token exists, add it to the request headers
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+})
+
+export default axiosWithAuth;
