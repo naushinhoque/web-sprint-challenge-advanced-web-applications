117d41426da91840efc00c9297e194233c2cb919
naushinhoque <fnmh@yahoo.com>
2023-10-10 20:25:39 -0500

    correctly made axiosWithAuth and imported

--- frontend/axios/index.js
+++ frontend/axios/index.js
@@ -1,43 +1,51 @@
 // ✨ implement axiosWithAuth
 import React from 'react';
-import { createRoot } from 'react-dom/client';
-import App from '../components/App';
+// import App from '../components/App';
 import axios from 'axios';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+// import { Routes, Route, Navigate } from 'react-router-dom';
+
+export const axiosWithAuth = () => {
+  const token = localStorage.getItem('token');
+
+  return axios.create({
+    baseURL: 'http://localhost:9000/api',
+    headers: {
+      authorization: token
+    }
+  });
+};
 
 
-const root = createRoot(document.getElementById('root'));
+// const axiosWithAuth = axios.create({
+//   baseURL: 'http://localhost:9000/api',
+//   headers: {
+//   Authorization: localStorage.getItem('token'),
+//     'Content-Type': 'application/json'
+//   }
+// });
 
-const axiosWithAuth = axios.create({
-  baseURL: 'http://localhost:9000/api',
-  headers: {
-    Authorization: localStorage.getItem('token'),
-    'Content-Type': 'application/json'
-  }
-});
+// axiosWithAuth.get('/articles')
+//   .then(response => {
+//     console.log('Authenticated GET arequest response',response.data)
 
-axiosWithAuth.get('/articles')
-  .then(response => {
-    console.log('Authenticated GET arequest response',response.data)
+//     // root.render(
+//     //   <React.StrictMode>
+//     //     <Router>
+//     //     <App axiosWithAuth={axiosWithAuth}/>
+//     //     </Router>
+//     //   </React.StrictMode>
+//     // );
+//   })
+//   .catch(error => {
+//     console.error('Authenticated GET request error', error);
 
-    root.render(
-      <React.StrictMode>
-        <Router>
-        <App axiosWithAuth={axiosWithAuth}/>
-        </Router>
-      </React.StrictMode>
-    );
-  })
-  .catch(error => {
-    console.error('Authenticated GET request error', error);
+//     // root.render(
+//     //   <React.StrictMode>
+//     //     <Router>
+//     //       <App axiosWithAuth={axiosWithAuth}/>
+//     //     </Router>  
+//     //   </React.StrictMode>
+//     // );
+//   })
 
-    root.render(
-      <React.StrictMode>
-        <Router>
-          <App axiosWithAuth={axiosWithAuth}/>
-        </Router>  
-      </React.StrictMode>
-    );
-  })
-
-  export default axiosWithAuth;
+  // export default axiosWithAuth;
--- frontend/components/Articles.js
+++ frontend/components/Articles.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react'
 import { Navigate } from 'react-router-dom'
 import PT from 'prop-types'
+import axiosWithAuth from '../axios/index';
 
 export default function Articles(props) {
   // ✨ where are my props? Destructure them here
